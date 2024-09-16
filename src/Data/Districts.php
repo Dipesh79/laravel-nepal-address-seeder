c4699afd92d6c157b56e575d5b65f498e2a7e62d
Dipesh Khanal <dipeshkhanal79@gmail.com>
2024-09-16 16:08:57 +0545

    nepali name added

--- src/Data/Districts.php
+++ src/Data/Districts.php
@@ -30,20 +30,20 @@ class Districts
     public function getProvinceOneDistricts(): array
     {
         return [
-            ['province_id' => 1, 'name' => 'Bhojpur', 'headquarter' => 'Bhojpur'],
-            ['province_id' => 1, 'name' => 'Dhankuta', 'headquarter' => 'Dhankuta'],
-            ['province_id' => 1, 'name' => 'Ilam', 'headquarter' => 'Ilam'],
-            ['province_id' => 1, 'name' => 'Jhapa', 'headquarter' => 'Bhadrapur'],
-            ['province_id' => 1, 'name' => 'Khotang', 'headquarter' => 'Diktel'],
-            ['province_id' => 1, 'name' => 'Morang', 'headquarter' => 'Biratnagar'],
-            ['province_id' => 1, 'name' => 'Okhaldhunga', 'headquarter' => 'Siddhicharan'],
-            ['province_id' => 1, 'name' => 'Panchthar', 'headquarter' => 'Phidim'],
-            ['province_id' => 1, 'name' => 'Sankhuwasabha', 'headquarter' => 'Khandbari'],
-            ['province_id' => 1, 'name' => 'Solukhumbu', 'headquarter' => 'Solududhkunda'],
-            ['province_id' => 1, 'name' => 'Sunsari', 'headquarter' => 'Inaruwa'],
-            ['province_id' => 1, 'name' => 'Taplejung', 'headquarter' => 'Phungling'],
-            ['province_id' => 1, 'name' => 'Terhathum', 'headquarter' => 'Myanglung'],
-            ['province_id' => 1, 'name' => 'Udayapur', 'headquarter' => 'Triyuga']
+            ['province_id' => 1, 'name' => 'Bhojpur', 'name_np' => 'भोजपुर', 'headquarter' => 'Bhojpur'],
+            ['province_id' => 1, 'name' => 'Dhankuta', 'name_np' => 'धनकुटा', 'headquarter' => 'Dhankuta'],
+            ['province_id' => 1, 'name' => 'Ilam', 'name_np' => 'इलाम', 'headquarter' => 'Ilam'],
+            ['province_id' => 1, 'name' => 'Jhapa', 'name_np' => 'झापा', 'headquarter' => 'Bhadrapur'],
+            ['province_id' => 1, 'name' => 'Khotang', 'name_np' => 'खोटाङ', 'headquarter' => 'Diktel'],
+            ['province_id' => 1, 'name' => 'Morang', 'name_np' => 'मोरङ', 'headquarter' => 'Biratnagar'],
+            ['province_id' => 1, 'name' => 'Okhaldhunga', 'name_np' => 'ओखलढुङ्गा', 'headquarter' => 'Siddhicharan'],
+            ['province_id' => 1, 'name' => 'Panchthar', 'name_np' => 'पाँचथर', 'headquarter' => 'Phidim'],
+            ['province_id' => 1, 'name' => 'Sankhuwasabha', 'name_np' => 'सङ्खुवासभा', 'headquarter' => 'Khandbari'],
+            ['province_id' => 1, 'name' => 'Solukhumbu', 'name_np' => 'सोलुखुम्बु', 'headquarter' => 'Solududhkunda'],
+            ['province_id' => 1, 'name' => 'Sunsari', 'name_np' => 'सुनसरी', 'headquarter' => 'Inaruwa'],
+            ['province_id' => 1, 'name' => 'Taplejung', 'name_np' => 'ताप्लेजुङ', 'headquarter' => 'Phungling'],
+            ['province_id' => 1, 'name' => 'Terhathum', 'name_np' => 'तेह्रथुम', 'headquarter' => 'Myanglung'],
+            ['province_id' => 1, 'name' => 'Udayapur', 'name_np' => 'उदयपुर', 'headquarter' => 'Triyuga']
         ];
     }
 
@@ -55,14 +55,14 @@ class Districts
     public function getProvinceTwoDistricts(): array
     {
         return [
-            ['province_id' => 2, 'name' => 'Parsa', 'headquarter' => 'Birgunj'],
-            ['province_id' => 2, 'name' => 'Bara', 'headquarter' => 'Kalaiya'],
-            ['province_id' => 2, 'name' => 'Rautahat', 'headquarter' => 'Gaur'],
-            ['province_id' => 2, 'name' => 'Sarlahi', 'headquarter' => 'Malangwa'],
-            ['province_id' => 2, 'name' => 'Dhanusha', 'headquarter' => 'Janakpur'],
-            ['province_id' => 2, 'name' => 'Siraha', 'headquarter' => 'Siraha'],
-            ['province_id' => 2, 'name' => 'Mahottari', 'headquarter' => 'Jaleshwar'],
-            ['province_id' => 2, 'name' => 'Saptari', 'headquarter' => 'Rajbiraj']
+            ['province_id' => 2, 'name' => 'Parsa', 'name_np' => 'पर्सा', 'headquarter' => 'Birgunj'],
+            ['province_id' => 2, 'name' => 'Bara', 'name_np' => 'बारा', 'headquarter' => 'Kalaiya'],
+            ['province_id' => 2, 'name' => 'Rautahat', 'name_np' => 'रौतहट', 'headquarter' => 'Gaur'],
+            ['province_id' => 2, 'name' => 'Sarlahi', 'name_np' => 'सर्लाही', 'headquarter' => 'Malangwa'],
+            ['province_id' => 2, 'name' => 'Dhanusha', 'name_np' => 'धनुषा', 'headquarter' => 'Janakpur'],
+            ['province_id' => 2, 'name' => 'Siraha', 'name_np' => 'सिराहा', 'headquarter' => 'Siraha'],
+            ['province_id' => 2, 'name' => 'Mahottari', 'name_np' => 'महोत्तरी', 'headquarter' => 'Jaleshwar'],
+            ['province_id' => 2, 'name' => 'Saptari', 'name_np' => 'सप्तरी', 'headquarter' => 'Rajbiraj']
         ];
     }
 
@@ -74,19 +74,19 @@ class Districts
     public function getProvinceThreeDistricts(): array
     {
         return [
-            ['province_id' => 3, 'name' => 'Sindhuli'],
-            ['province_id' => 3, 'name' => 'Ramechhap'],
-            ['province_id' => 3, 'name' => 'Dolakha'],
-            ['province_id' => 3, 'name' => 'Bhaktapur'],
-            ['province_id' => 3, 'name' => 'Dhading'],
-            ['province_id' => 3, 'name' => 'Kathmandu'],
-            ['province_id' => 3, 'name' => 'Kavrepalanchok'],
-            ['province_id' => 3, 'name' => 'Lalitpur'],
-            ['province_id' => 3, 'name' => 'Nuwakot'],
-            ['province_id' => 3, 'name' => 'Rasuwa'],
-            ['province_id' => 3, 'name' => 'Sindhupalchok'],
-            ['province_id' => 3, 'name' => 'Chitwan'],
-            ['province_id' => 3, 'name' => 'Makawanpur']
+            ['province_id' => 3, 'name' => 'Sindhuli', 'name_np' => 'सिन्धुली'],
+            ['province_id' => 3, 'name' => 'Ramechhap', 'name_np' => 'रामेछाप'],
+            ['province_id' => 3, 'name' => 'Dolakha', 'name_np' => 'दोलखा'],
+            ['province_id' => 3, 'name' => 'Bhaktapur', 'name_np' => 'भक्तपुर'],
+            ['province_id' => 3, 'name' => 'Dhading', 'name_np' => 'धादिङ'],
+            ['province_id' => 3, 'name' => 'Kathmandu', 'name_np' => 'काठमाडौं'],
+            ['province_id' => 3, 'name' => 'Kavrepalanchok', 'name_np' => 'काभ्रेपलाञ्चोक'],
+            ['province_id' => 3, 'name' => 'Lalitpur', 'name_np' => 'ललितपुर'],
+            ['province_id' => 3, 'name' => 'Nuwakot', 'name_np' => 'नुवाकोट'],
+            ['province_id' => 3, 'name' => 'Rasuwa', 'name_np' => 'रसुवा'],
+            ['province_id' => 3, 'name' => 'Sindhupalchok', 'name_np' => 'सिन्धुपाल्चोक'],
+            ['province_id' => 3, 'name' => 'Chitwan', 'name_np' => 'चितवन'],
+            ['province_id' => 3, 'name' => 'Makawanpur', 'name_np' => 'मकवानपुर']
         ];
     }
 
@@ -98,17 +98,17 @@ class Districts
     public function getProvinceFourDistricts(): array
     {
         return [
-            ['province_id' => 4, 'name' => 'Baglung'],
-            ['province_id' => 4, 'name' => 'Gorkha'],
-            ['province_id' => 4, 'name' => 'Kaski'],
-            ['province_id' => 4, 'name' => 'Lamjung'],
-            ['province_id' => 4, 'name' => 'Manang'],
-            ['province_id' => 4, 'name' => 'Mustang'],
-            ['province_id' => 4, 'name' => 'Myagdi'],
-            ['province_id' => 4, 'name' => 'Nawalpur'],
-            ['province_id' => 4, 'name' => 'Parbat'],
-            ['province_id' => 4, 'name' => 'Syangja'],
-            ['province_id' => 4, 'name' => 'Tanahun']
+            ['province_id' => 4, 'name' => 'Baglung', 'name_np' => 'बागलुङ'],
+            ['province_id' => 4, 'name' => 'Gorkha', 'name_np' => 'गोरखा'],
+            ['province_id' => 4, 'name' => 'Kaski', 'name_np' => 'कास्की'],
+            ['province_id' => 4, 'name' => 'Lamjung', 'name_np' => 'लमजुङ'],
+            ['province_id' => 4, 'name' => 'Manang', 'name_np' => 'मनाङ'],
+            ['province_id' => 4, 'name' => 'Mustang', 'name_np' => 'मुस्ताङ'],
+            ['province_id' => 4, 'name' => 'Myagdi', 'name_np' => 'म्याग्दी'],
+            ['province_id' => 4, 'name' => 'Nawalpur', 'name_np' => 'नवलपरासी'],
+            ['province_id' => 4, 'name' => 'Parbat', 'name_np' => 'पर्वत'],
+            ['province_id' => 4, 'name' => 'Syangja', 'name_np' => 'स्याङ्जा'],
+            ['province_id' => 4, 'name' => 'Tanahu', 'name_np' => 'तनहुँ']
         ];
     }
 
@@ -120,18 +120,18 @@ class Districts
     public function getProvinceFiveDistricts(): array
     {
         return [
-            ['province_id' => 5, 'name' => 'Kapilvastu'],
-            ['province_id' => 5, 'name' => 'Parasi'],
-            ['province_id' => 5, 'name' => 'Rupandehi'],
-            ['province_id' => 5, 'name' => 'Arghakhanchi'],
-            ['province_id' => 5, 'name' => 'Gulmi'],
-            ['province_id' => 5, 'name' => 'Palpa'],
-            ['province_id' => 5, 'name' => 'Dang'],
-            ['province_id' => 5, 'name' => 'Pyuthan'],
-            ['province_id' => 5, 'name' => 'Rolpa'],
-            ['province_id' => 5, 'name' => 'Eastern Rukum'],
-            ['province_id' => 5, 'name' => 'Banke'],
-            ['province_id' => 5, 'name' => 'Bardiya']
+            ['province_id' => 5, 'name' => 'Kapilvastu', 'name_np' => 'कपिलवस्तु'],
+            ['province_id' => 5, 'name' => 'Parasi', 'name_np' => 'परासी'],
+            ['province_id' => 5, 'name' => 'Rupandehi', 'name_np' => 'रुपन्देही'],
+            ['province_id' => 5, 'name' => 'Arghakhanchi', 'name_np' => 'अर्घाखाँची'],
+            ['province_id' => 5, 'name' => 'Gulmi', 'name_np' => 'गुल्मी'],
+            ['province_id' => 5, 'name' => 'Palpa', 'name_np' => 'पाल्पा'],
+            ['province_id' => 5, 'name' => 'Dang', 'name_np' => 'दा��'],
+            ['province_id' => 5, 'name' => 'Pyuthan', 'name_np' => 'प्युठान'],
+            ['province_id' => 5, 'name' => 'Rolpa', 'name_np' => 'रोल्पा'],
+            ['province_id' => 5, 'name' => 'Eastern Rukum', 'name_np' => 'पूर्वी रुकुम'],
+            ['province_id' => 5, 'name' => 'Banke', 'name_np' => 'बाँके'],
+            ['province_id' => 5, 'name' => 'Bardiya', 'name_np' => 'बर्दिया']
         ];
     }
 
@@ -143,16 +143,16 @@ class Districts
     public function getProvinceSixDistricts(): array
     {
         return [
-            ['province_id' => 6, 'name' => 'Western Rukum'],
-            ['province_id' => 6, 'name' => 'Salyan'],
-            ['province_id' => 6, 'name' => 'Dolpa'],
-            ['province_id' => 6, 'name' => 'Humla'],
-            ['province_id' => 6, 'name' => 'Jumla'],
-            ['province_id' => 6, 'name' => 'Kalikot'],
-            ['province_id' => 6, 'name' => 'Mugu'],
-            ['province_id' => 6, 'name' => 'Surkhet'],
-            ['province_id' => 6, 'name' => 'Dailekh'],
-            ['province_id' => 6, 'name' => 'Jajarkot']
+            ['province_id' => 6, 'name' => 'Western Rukum', 'name_np' => 'पश्चिमी रुकुम'],
+            ['province_id' => 6, 'name' => 'Salyan', 'name_np' => 'सल्यान'],
+            ['province_id' => 6, 'name' => 'Dolpa', 'name_np' => 'डोल्पा'],
+            ['province_id' => 6, 'name' => 'Humla', 'name_np' => 'हुम्ला'],
+            ['province_id' => 6, 'name' => 'Jumla', 'name_np' => 'जुम्ला'],
+            ['province_id' => 6, 'name' => 'Kalikot', 'name_np' => 'कालिकोट'],
+            ['province_id' => 6, 'name' => 'Mugu', 'name_np' => 'मुगु'],
+            ['province_id' => 6, 'name' => 'Surkhet', 'name_np' => 'सुर्खेत'],
+            ['province_id' => 6, 'name' => 'Dailekh', 'name_np' => 'दैलेख'],
+            ['province_id' => 6, 'name' => 'Jajarkot', 'name_np' => 'जाजरकोट']
         ];
     }
 
@@ -164,15 +164,15 @@ class Districts
     public function getProvinceSevenDistricts(): array
     {
         return [
-            ['province_id' => 7, 'name' => 'Kailali'],
-            ['province_id' => 7, 'name' => 'Achham'],
-            ['province_id' => 7, 'name' => 'Doti'],
-            ['province_id' => 7, 'name' => 'Bajhang'],
-            ['province_id' => 7, 'name' => 'Bajura'],
-            ['province_id' => 7, 'name' => 'Kanchanpur'],
-            ['province_id' => 7, 'name' => 'Dadeldhura'],
-            ['province_id' => 7, 'name' => 'Baitadi'],
-            ['province_id' => 7, 'name' => 'Darchula']
+            ['province_id' => 7, 'name' => 'Kailali', 'name_np' => 'कैलाली'],
+            ['province_id' => 7, 'name' => 'Achham', 'name_np' => 'अछाम'],
+            ['province_id' => 7, 'name' => 'Doti', 'name_np' => 'डोटी'],
+            ['province_id' => 7, 'name' => 'Bajhang', 'name_np' => 'बझाङ'],
+            ['province_id' => 7, 'name' => 'Bajura', 'name_np' => 'बाजुरा'],
+            ['province_id' => 7, 'name' => 'Kanchanpur', 'name_np' => 'कञ्चनपुर'],
+            ['province_id' => 7, 'name' => 'Dadeldhura', 'name_np' => 'डडेल्धुरा'],
+            ['province_id' => 7, 'name' => 'Baitadi', 'name_np' => 'बैतडी'],
+            ['province_id' => 7, 'name' => 'Darchula', 'name_np' => 'दार्चुला']
         ];
     }
 }
--- src/Data/Provinces.php
+++ src/Data/Provinces.php
@@ -12,13 +12,13 @@ class Provinces
     public function getProvinces(): array
     {
         return [
-            ['name' => 'Koshi', 'capital' => 'Biratnagar', 'no_of_districts' => 14,],
-            ['name' => 'Madhesh', 'capital' => 'Janakpur', 'no_of_districts' => 8,],
-            ['name' => 'Bagmati', 'capital' => 'Hetauda', 'no_of_districts' => 13,],
-            ['name' => 'Gandaki', 'capital' => 'Pokhara', 'no_of_districts' => 11,],
-            ['name' => 'Lumbini', 'capital' => 'Deukhuri', 'no_of_districts' => 12,],
-            ['name' => 'Karnali', 'capital' => 'Birendranagar', 'no_of_districts' => 10,],
-            ['name' => 'Sudurpashchim', 'capital' => 'Godawari', 'no_of_districts' => 9,]
+            ['name' => 'Koshi','name_np'=>'कोशी', 'capital' => 'Biratnagar', 'no_of_districts' => 14,],
+            ['name' => 'Madhesh','name_np'=>'मधेश', 'capital' => 'Janakpur', 'no_of_districts' => 8,],
+            ['name' => 'Bagmati','name_np'=>'बागमती', 'capital' => 'Hetauda', 'no_of_districts' => 13,],
+            ['name' => 'Gandaki','name_np'=>'गण्डकी', 'capital' => 'Pokhara', 'no_of_districts' => 11,],
+            ['name' => 'Lumbini','name_np'=>'लुम्बिनी', 'capital' => 'Deukhuri', 'no_of_districts' => 12,],
+            ['name' => 'Karnali','name_np'=>'कर्णाली', 'capital' => 'Birendranagar', 'no_of_districts' => 10,],
+            ['name' => 'Sudurpashchim','name_np'=>'सुदूरपश्चिम', 'capital' => 'Godawari', 'no_of_districts' => 9,]
         ];
     }
 
